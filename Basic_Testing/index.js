0bb883b231887e6d8bb33de5a0ab7ffbe36f2481
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-04-13 02:08:32 +0530

    just testing the basic string operations

---
+++ Basic_Testing/index.js
@@ -0,0 +1,7 @@
+let a = [];
+console.log(a.length);
+
+let string = "Hello there. This string is for just testing the string split function in a different way";
+let wordarray = string.split(" ");
+
+console.log([...wordarray]);
