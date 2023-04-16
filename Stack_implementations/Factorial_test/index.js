576c3675424ab8776bf3071b6c9bea88a9590dd6
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-04-16 10:19:00 +0530

    Tested how the factorial works

---
+++ Stack_implementations/Factorial_test/index.js
@@ -0,0 +1,45 @@
+function Stack(){
+    this.contents = [];
+    this.top = 0;
+    this.push = push;
+    this.peek = peek;
+    this.pop = pop;
+    this.clear = clear;
+    this.length = length;
+}
+
+
+function push(element){
+   this.contents[this.top++]= element;
+}
+function peek(){
+    return this.contents[this.top-1];
+}
+
+function pop(){
+    return this.contents[--this.top];
+}
+
+function clear(){
+    this.top =0;
+}
+function length(){
+    return this.top;
+}
+
+
+function factorial(number){
+    let c = new Stack();
+    for (let q =1; q<=number ; q++)
+    {
+        c.push(q);
+    }
+    let fac=1;
+    while(c.length()>0)
+    {
+        fac *= c.pop();
+    }
+    console.log(fac);
+}
+
+factorial(5);
